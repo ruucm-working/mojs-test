d8442611b573652cb858c4b8213f6f58b1488363
ruucm <ruucm.a@gmail.com>
2017-11-07 21:32:03 +0900

    update basic mo js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert('안녕');
+var shape2 = new mojs.Shape({
+	shape: 'circle',
+	scale: { 0: 1 },
+	duration: 1000,
+	delay: 1000,
+	easing: 'cubic.out',
+	repeat: 999
+}).play();
+var square = document.querySelector('#js-square');
+console.log('square : ');
+console.log(square);
+new mojs.Tween({
+	repeat: 999,
+	delay: 2000,
+	onUpdate: function(progress) {
+		square.style.transform = 'translateY(' + 200 * progress + 'px)';
+	}
+}).play();
